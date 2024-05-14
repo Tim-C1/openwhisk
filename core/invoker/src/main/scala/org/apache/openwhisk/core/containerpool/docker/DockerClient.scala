8e421d967247bfc8a724041ec047fcb4e8d659a0
Tim-C1 <chentingrun1011@163.com>
2024-05-14 20:42:39 +0800

    run python program on occlum-he openwhisk

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerClient.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerClient.scala
@@ -136,7 +136,7 @@ class DockerClient(dockerHost: Option[String] = None,
     }.flatMap { _ =>
       // Iff the semaphore was acquired successfully
       runCmd(
-        Seq("run", "-d") ++ args ++ Seq(image),
+        Seq("run", "-d", "--device=/dev/hyperenclave") ++ args ++ Seq(image),
         config.timeouts.run,
         if (config.maskDockerRunArgs) Some(Seq("run", "-d", "**ARGUMENTS HIDDEN**", image)) else None)
         .andThen {
